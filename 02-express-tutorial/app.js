1168608df51d03e104d784577f90ab5bbe98f741
sachin <sachinpantha69@gmail.com>
2022-03-07 20:01:16 +0545

    pushing explicitly

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,6 +1,11 @@
 const http=require('http');
 const {readFileSync}=require('fs');
-const homepage = readFileSync('./index.html');
+//LETS GRAB ALL THE NECESSARY FILES
+const homepage = readFileSync('./navbar-app/index.html');
+const homeStyles = readFileSync('./navbar-app/styles.css');
+const homeImage = readFileSync('./navbar-app/logo.svg');
+const homeLogic = readFileSync('./navbar-app/browser-app.js');
+
 const server=http.createServer((req,res)=>{
     const url=req.url;
     if(url==='/'){
@@ -10,6 +15,20 @@ const server=http.createServer((req,res)=>{
     else if(url==='/about'){
         res.writeHead(200,{'content-type':'text/html'});
         res.end('<h1>About page</h1>');
+    }
+    //TAKING STYLESHEETS
+    else if(url==='/styles.css'){
+        res.writeHead(200,{'content-type':'text/css'});
+        res.end(homeStyles)
+    }
+    //CSS FILES MUST BE TESTED EXPLICITLY IN ORDER TO RENDER IN DOM
+    else if(url==='/logo.svg'){
+        res.writeHead(200,{'content-type':'image/svg+xml'});
+        res.end(homeImage)
+    }
+    else if(url==='/browser-app.js'){
+        res.writeHead(200,{'content-type':'text/javascript'});
+        res.end(homeLogic)
     }
     else{
         //Sending 404 status code if not found
