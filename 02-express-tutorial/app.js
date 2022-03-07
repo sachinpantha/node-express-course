1346b155442090833d2e730b08b6a6a6ff23d61b
sachin <sachinpantha69@gmail.com>
2022-03-07 18:21:52 +0545

    amded hue hue

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,5 +1,17 @@
 const http=require('http');
 const server=http.createServer((req,res)=>{
-    res.writeHead(200,{'content-type':'text/html'});
-    res.end('<h1>Hello World</h1>');
+    const url=req.url;
+    if(url==='/'){
+        res.writeHead(200,{'content-type':'text/html'});
+        res.end('<h1>Home Page</h1>');
+    }
+    else if(url==='/about'){
+        res.writeHead(200,{'content-type':'text/html'});
+        res.end('<h1>About page</h1>');
+    }
+    else{
+        //Sending 404 status code if not found
+        res.writeHead(404,{'content-type':'text/html'});
+        res.end('<h1>Page not Found sed</h1>');
+    }
 }).listen(5000);
