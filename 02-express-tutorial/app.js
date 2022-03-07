28a5d9197958ca9593600accd48488b4e5fa568e
sachin <sachinpantha69@gmail.com>
2022-03-07 18:39:28 +0545

    External File link

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,9 +1,11 @@
 const http=require('http');
+const {readFileSync}=require('fs');
+const homepage = readFileSync('./index.html');
 const server=http.createServer((req,res)=>{
     const url=req.url;
     if(url==='/'){
         res.writeHead(200,{'content-type':'text/html'});
-        res.end('<h1>Home Page</h1>');
+        res.end(homepage);
     }
     else if(url==='/about'){
         res.writeHead(200,{'content-type':'text/html'});
