7212c21b40d417ca62875ca7ed998631f7caa4cd
sachin <sachinpantha69@gmail.com>
2022-03-12 08:01:49 +0545

    pushed

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -198,10 +198,18 @@ const express = require("express");
 const app = express();
 let { people } = require("./data");
 app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
 app.use(express.static('./methods-public'));
 app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, data: people });
 });
+app.post('/api/people',(req,res)=>{
+  const {name}=req.body;
+  if(!name){
+    return res.status(400).send('Please provide the value');
+  }
+  res.status(201).json({success:true,person:name})
+})
 app.post('/login',(req,res)=>{
    const {name}=req.body
    if(name){
